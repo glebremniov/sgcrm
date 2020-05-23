aeb7d26d4fc9af2c1502cac321b7ccafff50ffad
glebremiov <remniov.work@gmail.com>
2020-05-23 18:38:14 +0300

    FEAT: update Address domain class

--- server/grails-app/domain/address/Address.groovy
+++ server/grails-app/domain/address/Address.groovy
@@ -8,21 +8,15 @@ class Address {
 
     String country
     String countryISO2code
-    String state
     String city
     String postcode
-    String street
-    String buildingsNumber
-    String office
+    String addressString
 
     static constraints = {
         country nullable: true, size: 0..50
         countryISO2code size: 2..2, inList: CountryCode.values()*.alpha2
-        state size: 1..100
         city size: 1..100
         postcode size: 5..10
-        street size: 1..100
-        buildingsNumber size: 1..10
-        office nullable: true, size: 1..10
+        addressString size: 1..200
     }
 }
