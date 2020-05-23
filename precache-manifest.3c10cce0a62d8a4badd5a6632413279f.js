a74e2f67921f659741cca4dd6471df65782767a9
glebremiov <remniov.work@gmail.com>
2020-05-23 13:15:28 +0300

    Updates

---
+++ precache-manifest.3c10cce0a62d8a4badd5a6632413279f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d5b0aa30799ccdb854417080a86af5",
+    "url": "/sgcrm/index.html"
+  },
+  {
+    "revision": "cc559579c3756f981306",
+    "url": "/sgcrm/static/css/main.2d9a02c3.chunk.css"
+  },
+  {
+    "revision": "16a925a033bdb60f4e12",
+    "url": "/sgcrm/static/js/2.0609ed99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sgcrm/static/js/2.0609ed99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc559579c3756f981306",
+    "url": "/sgcrm/static/js/main.dc455b45.chunk.js"
+  },
+  {
+    "revision": "9c92d29fff447c106197",
+    "url": "/sgcrm/static/js/runtime-main.c5efbb74.js"
+  },
+  {
+    "revision": "248c6acfda2681fc8408d1ea0007e932",
+    "url": "/sgcrm/static/media/Dual Ball-0.9s-200px.248c6acf.svg"
+  }
+]);
