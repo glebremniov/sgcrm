73ff90dba03fc8d799203a0067ac1c973957bbbc
glebremiov <remniov.work@gmail.com>
2020-05-28 10:49:55 +0300

    FEAT: implement CalendarDetailsTable

--- client/src/components/CalendarDetails/CalendarDetails.js
+++ client/src/components/CalendarDetails/CalendarDetails.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./CalendarDetails.css";
 import Loader from "../Loader/Loader";
+import {Alert} from "react-bootstrap";
+import CalendarDetailsTable from "../CalendarDetailsTable/CalendarDetailsTable";
 
-const CalendarDetails = ({data = []}) => {
-    if (data.length === 0) {
+const CalendarDetails = ({data}) => {
+    if (!data) {
         return <Loader/>
+    }
+
+    if (data.length === 0) {
+        return (
+            <Alert variant="light">
+                <Alert.Heading>В Вашем календаре события не найдены.</Alert.Heading>
+            </Alert>
+        )
     }
 
     return (
         <div className="calendar-details">
-
+            <CalendarDetailsTable data={data}/>
         </div>
     )
 }
